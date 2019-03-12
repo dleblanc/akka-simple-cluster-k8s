478b7db3aa79c87e920700242c2f553d619d98d8
Dave LeBlanc <david.leblanc@gmail.com>
2019-03-12 16:39:39 -0700

    Added a headless service
    This is supposedly necessary to resolve pod names.

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -3,4 +3,5 @@
 kubectl apply -f account.yaml
 kubectl apply -f budget.yaml
 kubectl apply -f config.yaml
+kubectl create -f headless-service.yaml --save-config
 kubectl create -f deployment.yaml --save-config
--- k8s/teardown.sh
+++ k8s/teardown.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 kubectl delete -f deployment.yaml
+kubectl delete -f headless-service.yaml
 kubectl delete -f account.yaml
 kubectl delete -f budget.yaml
 kubectl delete -f config.yaml
